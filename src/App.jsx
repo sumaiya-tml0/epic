33cb7a895b1a444de46a4497bb39a2e22b46660a
sumaiya <af.sumaiyaa@gmail.com>
2025-05-14 11:28:01 +0600

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 
-import { Button } from '@chakra-ui/react'
 import './App.css'
 import { Provider } from './components/ui/provider'
 import Header from './components/Header'
@@ -7,11 +6,11 @@ import Header from './components/Header'
 function App() {
 
   return (
-    <Provider>
+    <>
       
       <Header/>
       
-    </Provider>
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
 import { FaFacebook, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
 
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 function Header() {
+      const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className='flex items-center justify-between'>
-        <img src={logo} alt="" />
+    <header className="py-7">
+        <div className="max-w-[1300px] mx-auto px-4">
+            <div className="flex items-center justify-between">
+      <div className="max-w-40">
+        <img src={logo} alt="Epic group" />
+      </div>
 
-        <div>
-            <ul className='uppercase flex items-center gap-6 !text-xl text-gray-500'>
-                
-                <li><a href="">our company</a></li>
-                <li><a href="">our purpose</a></li>
-                <li><a href="">our capabilities</a></li>
-                <li><a href="">our career</a></li>
-            </ul>
-        </div>
-        <div>
-            <ul className='flex items-center gap-6 !text-xl'>
-                <li><a href=""><FaLinkedin />
-</a></li>
-                <li><a href=""><FaFacebook /></a></li>
-                <li><a href=""><FaInstagramSquare /></a></li>
-                <li><a href="">our career</a></li>
-            </ul>
-        </div>
-
-        
+      <div className="hidden lg:block">
+        <ul className="uppercase flex items-center gap-6 !lg:text-xl text-gray-500">
+          <li>
+            <a href="">our company</a>
+          </li>
+          <li>
+            <a href="">our purpose</a>
+          </li>
+          <li>
+            <a href="">our capabilities</a>
+          </li>
+          <li>
+            <a href="">our career</a>
+          </li>
+        </ul>
+      </div>
+      {/* Social Icons */}
+      <div>
+        <ul className="flex items-center gap-3 lg:!text-2xl text-blue-950">
+          <li>
+            <a href="">
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <FaInstagramSquare />
+            </a>
+          </li>
+         
+        </ul>
+      </div>
+       {/* Hamburger Menu (Mobile) */}
+          <div className="lg:hidden">
+            <button onClick={toggleMenu} className="text-2xl">
+              {menuOpen ? <HiOutlineX /> : <HiOutlineMenu />}
+            </button>
+          </div>
     </div>
-  )
+
+     {/* Mobile Navigation */}
+        {menuOpen && (
+          <div className="mt-4 lg:hidden">
+            <ul className="flex flex-col gap-4 uppercase text-gray-600 text-base">
+              <li><a href="">our company</a></li>
+              <li><a href="">our purpose</a></li>
+              <li><a href="">our capabilities</a></li>
+              <li><a href="">our careers</a></li>
+            </ul>
+          </div>
+        )}
+        </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
